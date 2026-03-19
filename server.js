c806cfec2a6412a4957943bf7d26af8d3fa66ccb
Ninjalegend88 <itstimenaruto@gmail.com>
2026-03-19 10:41:26 -0400

    Add files via upload

---
+++ server.js
@@ -0,0 +1,53 @@
+const express = require("express");
+const http = require("http");
+const path = require("path");
+const { createBareServer } = require("@tomphttp/bare-server-node");
+
+const app = express();
+const bare = createBareServer("/bare/");
+const PORT = process.env.PORT || 8080;
+
+app.use(
+  "/uv/",
+  express.static(
+    path.join(__dirname, "node_modules/@titaniumnetwork-dev/ultraviolet/dist")
+  )
+);
+
+app.get("/uv/uv.config.js", (req, res) => {
+  res.setHeader("Content-Type", "application/javascript");
+  res.send(`
+self.__uv$config = {
+  prefix: "/service/",
+  bare: "/bare/",
+  encodeUrl: Ultraviolet.codec.xor.encode,
+  decodeUrl: Ultraviolet.codec.xor.decode,
+  handler: "/uv/uv.handler.js",
+  bundle: "/uv/uv.bundle.js",
+  config: "/uv/uv.config.js",
+  sw: "/uv/uv.sw.js",
+};
+  `);
+});
+
+app.use(express.static(path.join(__dirname, "public")));
+
+const server = http.createServer((req, res) => {
+  if (bare.shouldRoute(req)) {
+    bare.routeRequest(req, res);
+  } else {
+    app(req, res);
+  }
+});
+
+server.on("upgrade", (req, socket, head) => {
+  if (bare.shouldRoute(req)) {
+    bare.routeUpgrade(req, socket, head);
+  } else {
+    socket.destroy();
+  }
+});
+
+server.listen(PORT, () => {
+  console.log(`✅ Running at http://localhost:${PORT}`);
+});
